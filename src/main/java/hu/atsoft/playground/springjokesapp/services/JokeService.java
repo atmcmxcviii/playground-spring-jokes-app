514a9de27195e7d1af2ed7f0f7694501d6ba20db
dp <hudomokp@gmail.com>
2019-02-08 21:04:11 +0100

    Created app

---
+++ src/main/java/hu/atsoft/playground/springjokesapp/services/JokeService.java
@@ -0,0 +1,6 @@
+package hu.atsoft.playground.springjokesapp.services;
+
+public interface JokeService {
+
+	String getJoke();
+}
---
+++ src/test/java/hu/atsoft/playground/springjokesapp/SpringJokesAppApplicationTests.java
@@ -0,0 +1,17 @@
+package hu.atsoft.playground.springjokesapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringJokesAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
