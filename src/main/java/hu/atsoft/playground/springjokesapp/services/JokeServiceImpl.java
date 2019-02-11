a40a84f5da089d90e57baad849d19e777ec36521
dp <hudomokp@gmail.com>
2019-02-11 18:27:37 +0100

    Use external class as bean using spring configuration

--- src/main/java/hu/atsoft/playground/springjokesapp/services/JokeServiceImpl.java
+++ src/main/java/hu/atsoft/playground/springjokesapp/services/JokeServiceImpl.java
@@ -1,5 +1,6 @@
 package hu.atsoft.playground.springjokesapp.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
@@ -7,10 +8,11 @@ import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 @Service
 public class JokeServiceImpl implements JokeService {
 	
+	@Autowired
 	private final ChuckNorrisQuotes chuckNorrisQuotes;
 	
-	public JokeServiceImpl() {
-		this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+	public JokeServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
+		this.chuckNorrisQuotes = chuckNorrisQuotes;
 	}
 
 	@Override
